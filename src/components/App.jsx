447800ef3d59d1c5e6f80629d448cd13ed094c2e
LarysaHliieva <lora.glievaya@gmail.com>
2023-07-07 18:53:49 +0300

    subtask1 finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,68 @@
 import { Component } from 'react';
 
+import { nanoid } from 'nanoid';
+
 import styles from './App.module.css';
 
 export class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: nanoid(),
+        name: 'Rosi',
+      },
+
+      {
+        id: nanoid(),
+        name: 'Eden',
+      },
+    ],
+    name: '',
+  };
+
+  addContact = e => {
+    e.preventDefault();
+
+    const { name } = this.state;
+    const newContact = {
+      id: nanoid(),
+      name,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+
+    this.resetForm();
+  };
+
+  resetForm() {
+    this.setState({
+      name: '',
+    });
+  }
+
+  hangleChange = e => {
+    const { value } = e.currentTarget;
+    this.setState({
+      name: value,
+    });
+  };
+
   render() {
+    const { contacts, name } = this.state;
     return (
       <div className={styles.phonebook}>
         <h2>Phonebook</h2>
-        <form className={styles.phonebook__form}>
+        <form onSubmit={this.addContact} className={styles.phonebook__form}>
           <label className={styles.label}>
             Name
             <input
               className={styles.input}
+              onChange={this.hangleChange}
               type="text"
               name="name"
+              value={name}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
@@ -26,9 +75,9 @@ export class App extends Component {
         </form>
         <h2>Contacts</h2>
         <ul>
-          <li>Rosi</li>
-          <li>Eden</li>
-          <li>Hermione</li>
+          {contacts.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
         </ul>
       </div>
     );
