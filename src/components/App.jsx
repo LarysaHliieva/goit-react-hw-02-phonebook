a5456b283e3dbaaf5e9b28ad5b079398f1f38075
LarysaHliieva <lora.glievaya@gmail.com>
2023-07-17 16:46:04 +0300

    finished filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,10 @@ import styles from './App.module.css';
 export class App extends Component {
   state = {
     contacts: [
-      {
-        id: nanoid(),
-        name: 'Rosi',
-        number: 111 - 11 - 11,
-      },
-
-      {
-        id: nanoid(),
-        name: 'Eden',
-        number: 222 - 22 - 22,
-      },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     name: '',
     number: '',
@@ -45,7 +38,6 @@ export class App extends Component {
     this.setState({
       name: '',
       number: '',
-      filter: '',
     });
   }
 
@@ -56,16 +48,26 @@ export class App extends Component {
     });
   };
 
-  render() {
-    const { contacts, name, number, filter } = this.state;
+  getFilteredContacts() {
+    const { contacts, filter } = this.state;
+
+    if (!filter) {
+      return contacts;
+    }
 
     const normalizedFilter = filter.toLowerCase();
 
-    const filteredContacts = contacts.filter(
+    return contacts.filter(
       item =>
         item.name.toLowerCase().includes(normalizedFilter) ||
-        String(item.number).toLowerCase().includes(normalizedFilter)
+        item.number.toLowerCase().includes(normalizedFilter)
     );
+  }
+
+  render() {
+    const { name, number, filter } = this.state;
+
+    const filteredContacts = this.getFilteredContacts();
 
     return (
       <div className={styles.phonebook}>
